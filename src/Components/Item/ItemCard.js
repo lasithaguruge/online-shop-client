d4e47bd42b3b6d6ac419f459cc53711d3385b3e2
Lasitha Guruge <lasithaguruge91@gmail.com>
2020-06-12 07:39:11 +0530

    Add item quantity update functionality

--- src/Components/Item/ItemCard.js
+++ src/Components/Item/ItemCard.js
@@ -1,41 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Item, Input, Grid, Dropdown } from 'semantic-ui-react';
+import { v4 as uuid4 } from 'uuid';
 
 const options = [
   { key: 1, text: 'Unit', value: 'unit' },
   { key: 2, text: 'Carton', value: 'carton' }
 ]
 
-export const ItemCard = props => {
-  const { item, uom = 'unit', onHandleChange, onHandleClickItemCount, onHandleAddToCart } = props;
+class ItemCard extends Component {
+  state = { 
+    quantity: 0,
+    uom: 'unit'
+  }
 
-  return <Grid columns={4} divided='vertically'>
-    <Grid.Row>
-      <Grid.Column>
-        <Item>
-          <Item.Content>
-            <Item.Header as='a'>{item.name}</Item.Header>
-            <Item.Description>{item.description}</Item.Description>
-          </Item.Content>
-        </Item>
-      </Grid.Column>
-      <Grid.Column style={{ marginTop: 5 }}>
-        <Grid columns={4}>
-          <Grid.Column width={2}><Button circular icon='plus' size='mini' onClick={(value) => onHandleClickItemCount('INCREASE', value)}/></Grid.Column>
-          <Grid.Column width={4}><Input size='mini' style={{ width: 64 }}/></Grid.Column>
-          <Grid.Column width={2}><Button circular icon='minus' size='mini' onClick={(value) => onHandleClickItemCount('DECREASE', value)}/></Grid.Column>
-          <Grid.Column style={{ margin: 5 }} width={7} >
-            <Dropdown options={options} value={uom} onChange={onHandleChange} />
-          </Grid.Column>
-        </Grid>
-      </Grid.Column>
-      <Grid.Column></Grid.Column>
-      <Grid.Column 
-        width={1} 
-        style={{ marginTop: 5, marginRight: 10 }} 
-        floated='right'
-      ><Button size='mini' onClick={onHandleAddToCart}>Add</Button>
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
+  onQuantityChange = value => {
+    this.setState({ quantity: this.state.quantity + value });
+  }
+
+  handleAddToCart = () => {
+    const { item, quantity, addToCart, updateQuantity } = this.props;
+
+    if (!this.orderItemId) {
+      this.orderItemId = uuid4();
+      addToCart(item, quantity, this.orderItemId);
+    } else {
+      updateQuantity(this.orderItemId, quantity);
+    }
+
+    this.handleResetItemState();
+  }
+
+  handleResetItemState = () => {
+    if (this.addToCartTimeout) clearTimeout(this.addToCartTimeout);
+
+    this.addToCartTimeout = setTimeout(() => {
+      this.orderItemId = null;
+      this.setState({ quantity: 0 });
+    }, 3000);
+  }
+
+  render() {
+    const { item, onHandleChange } = this.props;
+    const { quantity, uom } = this.state;
+
+    return <Grid columns={4} divided='vertically'>
+      <Grid.Row>
+        <Grid.Column>
+          <Item>
+            <Item.Content>
+              <Item.Header as='a'>{item.name}</Item.Header>
+              <Item.Description>{item.description}</Item.Description>
+            </Item.Content>
+          </Item>
+        </Grid.Column>
+        <Grid.Column style={{ marginTop: 5 }}>
+          <Grid columns={4}>
+            <Grid.Column width={2}><Button circular icon='plus' size='mini' onClick={(e) => this.onQuantityChange(1)} /></Grid.Column>
+            <Grid.Column width={4}><Input value={quantity} size='mini' style={{ width: 64 }} /></Grid.Column>
+            <Grid.Column width={2}><Button circular icon='minus' size='mini' onClick={(e) => quantity > 0 && this.onQuantityChange(-1)} /></Grid.Column>
+            <Grid.Column style={{ margin: 5 }} width={7} >
+              <Dropdown options={options} value={uom} onChange={(e) => onHandleChange('uom', e.target.value)} />
+            </Grid.Column>
+          </Grid>
+        </Grid.Column>
+        <Grid.Column></Grid.Column>
+        {/* <Grid.Column
+          width={1}
+          style={{ marginTop: 5, marginRight: 10 }}
+          floated='right'
+        ><Button size='mini' onClick={() => onHandleAddToCart(item.id)}>Add</Button>
+        </Grid.Column> */}
+      </Grid.Row>
+    </Grid>
+  }
 }
+
+export default ItemCard;
--- src/Containers/Items/index.js
+++ src/Containers/Items/index.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Divider, Header, List } from 'semantic-ui-react';
-import { ItemCard } from '../../Components/Item/ItemCard';
+import ItemCard from '../../Components/Item/ItemCard';
 import { getItems } from '../../redux/reducers/items';
 
 class ItemList extends Component {
+  state = { 
+    cartItems: [] 
+  }
+  
+  handleOnClickItemQuantityChange = (action, itemId, value) => {
+    console.log("handleOnClickItemQuantityChange ", action, itemId, value)
+
+    // const cartItem = this.state.cartItems.find(cartItem => cartItem.id ==)
+    // const item = this.props.items.find(item => item.id === itemId);
+
+    // if (item) {
+      
+    // } else {
+    //   this.createNewCartItem(item, value);
+    // }
+  }
+
+  handleOnChange = (key, itemId, value) => {
+    console.log("handleOnChange ", key,  itemId, value)
+  }
+
+  handleAddToCart = (itemId) => {
+    console.log("handleAddToCart ", itemId)
+  }
+
+  createNewCartItem = (itemId, quantity = 0, uom = 'unit') => {
+    const item = this.props.items.find(item => item.id === itemId);
+
+    return { id: 5, item, quantity, uom };
+  };
 
   render() {
     return (
@@ -12,7 +42,14 @@ class ItemList extends Component {
         <Header as='h3'>Products</Header>
         <Divider />
         <List verticalAlign='middle'>
-          {this.props.items.map(item => <ItemCard key={item.id} item={item} />)}
+          {this.props.items.map(item => {
+            return <ItemCard 
+              key={item.id} 
+              item={item}
+              onHandleClickItemQuantityChange={this.handleOnClickItemQuantityChange}
+              onHandleChange={this.handleOnChange}
+              onHandleAddToCart={this.handleAddToCart} />
+          })}
         </List>
       </div>
     )
