19280209194015598e8b0b2773dbeb80f09b5f76
Lasitha Guruge <lasithaguruge91@gmail.com>
2020-06-14 18:59:43 +0530

    Add search to price list

--- src/Containers/PriceList/index.js
+++ src/Containers/PriceList/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Divider, Dimmer, Header, List, Loader, Segment } from 'semantic-ui-react';
+import { Divider, Dimmer, Grid, Header, Input, List, Loader, Segment } from 'semantic-ui-react';
 import PriceListCard from '../../Components/Cards/PriceListCard';
 import { fetchPriceList } from '../../actions/items';
 import { getPriceListItems } from '../../redux/reducers';
 
 class ItemList extends Component {
   state = { 
-    loading: false
+    loading: false,
+    quantity: undefined,
+    name: undefined
   }
 
   componentDidMount = () => {
@@ -17,8 +19,38 @@ class ItemList extends Component {
     })
   }
 
+  onQuantityChange = event => {
+    if (event.target.validity.valid) {
+      this.setState({ quantity: event.target.value })
+    }
+  }
+
+  onNameChange = value => {
+    this.setState({ name: value })
+  }
+
+  filteredList = () => {
+    const { priceList = [] } = this.props;
+    const { name, quantity } = this.state;
+    let filteredItemsList = priceList;
+
+    if (name && name.length > 0) {
+      filteredItemsList = priceList.filter(item => {
+        return item && item.name.toLowerCase().includes(name.toLowerCase())
+      });
+    }
+
+    if (quantity) {
+      filteredItemsList = filteredItemsList.filter(item => {
+        return (item.noOfUnits === parseInt(quantity))
+      });
+    }
+
+    return filteredItemsList;
+  }
+
   render() {
-    const { loading } = this.state;
+    const { loading, quantity, name } = this.state;
     return (
       <Segment basic>
       <Dimmer active={loading} inverted>
@@ -27,9 +59,17 @@ class ItemList extends Component {
 
       <div>
         <Header as='h3'>Prices</Header>
+        <Grid columns={5} divided='vertically'>
+          <Grid.Row>
+            <Grid.Column><Input placeholder='Search by name' onChange={(e) => this.onNameChange(e.target.value)}/></Grid.Column>
+            <Grid.Column><Input placeholder='Search by quantity' type="text" pattern="[0-9]*" value={quantity} onChange={(e) => this.onQuantityChange(e)} /></Grid.Column>
+          </Grid.Row>
+        </Grid>
+        
+        
         <Divider />
         <List verticalAlign='middle'>
-          {this.props.priceList.map(item => {
+          {this.filteredList().map(item => {
             return <PriceListCard 
               key={item.id}
               item={item} />
